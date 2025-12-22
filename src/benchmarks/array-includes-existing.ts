4830a0af119bb779f9dbd06c9a695bb4097ea6bf
James Garbutt <43081j@users.noreply.github.com>
2025-12-22 14:01:35 +0000

    feat: add includes benchmarks

---
+++ src/benchmarks/array-includes-existing.ts
@@ -0,0 +1,10 @@
+const data = Array.from({length: 1000}, (_, i) => i);
+const needle = 500;
+
+export function indexOf() {
+  return data.indexOf(needle) !== -1;
+}
+
+export function includes() {
+  return data.includes(needle);
+}
---
+++ src/benchmarks/string-includes-existing.ts
@@ -0,0 +1,11 @@
+const data =
+  'oranges are almost as good as apples, if apples were oranges'.repeat(100);
+const needle = 'apples';
+
+export function indexOf() {
+  return data.indexOf(needle) !== -1;
+}
+
+export function includes() {
+  return data.includes(needle);
+}
